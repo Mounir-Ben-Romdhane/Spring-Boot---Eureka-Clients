dfbf179ac0341acb2133bf66264c53226ceda71f
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2023-11-06 16:40:10 +0100

    First commit

---
+++ src/test/java/com/example/mscondidate/MsCondidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscondidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCondidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
