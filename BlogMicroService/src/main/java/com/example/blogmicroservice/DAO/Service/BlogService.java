4f6c959a1d7f7735a8c5f5c22b3aecc3884baff3
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2023-12-10 12:26:27 +0100

    emnablog

--- BlogMicroService/src/main/java/com/example/blogmicroservice/DAO/Service/BlogService.java
+++ BlogMicroService/src/main/java/com/example/blogmicroservice/DAO/Service/BlogService.java
@@ -5,6 +5,8 @@ import com.example.blogmicroservice.DAO.Repositories.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BlogService implements  IBlogService{
 
@@ -14,6 +16,31 @@ BlogRepository blogRepository;
     @Override
     public Blog addBlog(Blog b) {
         return blogRepository.save(b);
+    }
+
+    @Override
+    public Blog editBlog(Long id , Blog b) {
+        return blogRepository.save(b);
+    }
+
+    @Override
+    public List<Blog> findAll() {
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public Blog findAllById(long id) {
+        return blogRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(long id) {
+       blogRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Blog b) {
+        blogRepository.delete(b);
     }
 }
 
